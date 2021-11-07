b4f1e69a71e3c5d94ec3e11c7761959ee9e6fb6c
AZIZXlaouiti <azizrocke@gmail.com>
2021-11-06 21:26:44 -0400

    adding map generation

--- core/level.py
+++ core/level.py
@@ -1,14 +1,20 @@
 import pygame 
 from tiles import Tile
-
+from settings import *
 class Level:
     def __init__(self, level_map ,surface ):
         self.surface = surface
         self.setup_level(level_map)
     def setup_level(self, layout):
         self.tiles = pygame.sprite.Group()
-        for row in layout:
-            print(row)
+        for index_row , row in enumerate(layout):
+            for index_col ,col in enumerate(row): 
+               print(index_row, index_col,col)
+               if col == 'X':
+                   y = index_row * tile_size
+                   x = index_col * tile_size
+                   tile = Tile((x,y),tile_size)
+                   self.tiles.add(tile)
 
     def run(self):
         self.tiles.draw(self.surface)
