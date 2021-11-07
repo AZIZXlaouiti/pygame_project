82540ce1921e8e40fb41b83b2a8d9900ec7537a8
AZIZXlaouiti <azizrocke@gmail.com>
2021-11-07 14:37:26 -0500

    small change

--- core/player.py
+++ core/player.py
@@ -6,13 +6,13 @@ class Player(pygame.sprite.Sprite):
        self.load_animation()
        self.frame = 0
        self.image = self.animation["run"][self.frame]
-       self.rect = self.image.get_rect()
+       self.rect = self.image.get_rect(topleft = pos)
        self.direction = pygame.math.Vector2(0,0)
        self.speed = 1
        self.gravity = 0.8
        self.jump_speed = -10
        self.last_update = pygame.time.get_ticks()
-       self.frame_rate = 70
+       self.frame_rate = 0.15
     def get_input(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_RIGHT]:
@@ -37,13 +37,16 @@ class Player(pygame.sprite.Sprite):
         for i in range (4):
             self.animation["run"].append(right.parse_sprite(f'frame_{i}.png'))  
     def animate(self):
-        now  = pygame.time.get_ticks()
-        if now - self.last_update > self.frame_rate:
-            self.last_update = now
-            self.frame += 1
-            if self.frame >= len(self.animation["run"]):
-                self.frame = 0
+        # now  = pygame.time.get_ticks()
+        # if now - self.last_update > self.frame_rate:
+        #     self.last_update = now
+        #     self.frame += 1
+        #     if self.frame >= len(self.animation["run"]):
+        #         self.frame = 0
+        self.frame = (self.frame + 0.142)% len(self.animation['run'])
+        self.image = self.animation['run'][int(self.frame)]
+
         # x,y = self.direction.x,self.direction.y
-        self.image = self.animation['run'][self.frame]
+        # self.image = self.animation['run'][self.frame]
         # self.rect = self.image.get_rect()
         # self.direction.x,self.direction.y = x , y
