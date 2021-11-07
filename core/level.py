f11ab759895686a7760ea52383181b453952e5d8
AZIZXlaouiti <azizrocke@gmail.com>
2021-11-06 22:03:06 -0400

    adding player instance

--- core/level.py
+++ core/level.py
@@ -1,4 +1,5 @@
-import pygame 
+import pygame
+from player import Player
 from tiles import Tile
 from settings import *
 class Level:
@@ -8,15 +9,19 @@ class Level:
         self.world_shift = 0
     def setup_level(self, layout):
         self.tiles = pygame.sprite.Group()
+        self.player = pygame.sprite.GroupSingle()
         for index_row , row in enumerate(layout):
             for index_col ,col in enumerate(row): 
-               print(index_row, index_col,col)
                if col == 'X':
                    y = index_row * tile_size
                    x = index_col * tile_size
                    tile = Tile((x,y),tile_size)
                    self.tiles.add(tile)
-
+               if col == 'P':
+                   y = index_row * tile_size
+                   x = index_col * tile_size
+                   player_sprite = Player((x,y))
+                   self.tiles.add(player_sprite)
     def run(self):
         #adding camera by offsetting the world(i.e sprite Group) 
         self.tiles.update(self.world_shift)
--- core/settings.py
+++ core/settings.py
@@ -1,15 +1,15 @@
 level_map = [
-'                            ',
-'                            ',
-'                            ',
-' XX    XXX            XX    ',
-' XX P                       ',
-' XXXX        X          XX  ',
-' XX         XX              ',
-' XX       XXXX    XX  XX    ',
-'      XXXXXXXXXXXXXXXXXXXXXX',
-'                            ',
-'XXXXXXXXXXXXXXXXXXXXXXXXXXXX']
+    '                            ',
+    '                            ',
+    '                            ',
+    ' XX    XXX            XX    ',
+    ' XX P                       ',
+    ' XXXX        X          XX  ',
+    ' XX         XX              ',
+    ' XX       XXXX    XX  XX    ',
+    '      XXXXXXXXXXXXXXXXXXXXXX',
+    '                            ',
+    'XXXXXXXXXXXXXXXXXXXXXXXXXXXX']
 tile_size = 64
 HEIGHT = len(level_map) * tile_size
 WIDTH = 1200
